94efbe161b925187ad571d286d7b341365b4f4b4
Bitz3d <raf.cieslinski@gmail.com>
2019-02-22 20:46:18 +0100

    praca wre

--- src/main/java/pl/rafalab/xmlReader/Utils/Interfaces/SeleniumTestXmlLoader.java
+++ src/main/java/pl/rafalab/xmlReader/Utils/Interfaces/SeleniumTestXmlLoader.java
@@ -1,7 +1,6 @@
 package pl.rafalab.xmlReader.Utils.Interfaces;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 import pl.rafalab.xmlReader.Model.Testrun;
 
 import java.io.File;
--- src/main/java/pl/rafalab/xmlReader/XmlReaderApplication.java
+++ src/main/java/pl/rafalab/xmlReader/XmlReaderApplication.java
@@ -2,8 +2,10 @@ package pl.rafalab.xmlReader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class XmlReaderApplication {
 
 	public static void main(String[] args) {
