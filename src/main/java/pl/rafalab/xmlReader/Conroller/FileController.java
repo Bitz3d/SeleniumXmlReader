b0db2833029b6b41fb319a5c066dc8a714722f15
Bitz3d <raf.cieslinski@gmail.com>
2019-02-22 21:29:03 +0100

    sss

--- src/main/java/pl/rafalab/xmlReader/Conroller/FileController.java
+++ src/main/java/pl/rafalab/xmlReader/Conroller/FileController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.rafalab.xmlReader.Model.Test;
 import pl.rafalab.xmlReader.Model.Testrun;
 
 import java.util.List;
@@ -14,6 +15,13 @@ import java.util.List;
 @RequestMapping("/api/")
 public class FileController {
 
+
+    Test test;
+
+    @Autowired
+    public FileController(Test test) {
+        this.test = test;
+    }
 
     @GetMapping("all/files")
     ResponseEntity<String> getAllFiles(){
---
+++ src/main/java/pl/rafalab/xmlReader/Utils/Test.java
@@ -0,0 +1,12 @@
+package pl.rafalab.xmlReader.Utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+    public String siema(){
+        return "siema from Test";
+    }
+
+}
