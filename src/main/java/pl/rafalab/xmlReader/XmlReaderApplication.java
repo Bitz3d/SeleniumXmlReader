fd1ddab5b5076f7ea08ee0d4c640a44b394d35d8
Bitz3d <raf.cieslinski@gmail.com>
2019-02-24 12:00:17 +0100

    sd

--- src/main/java/pl/rafalab/xmlReader/XmlReaderApplication.java
+++ src/main/java/pl/rafalab/xmlReader/XmlReaderApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("pl.rafalab.xmlReader.Utils")
 public class XmlReaderApplication {
 
 	public static void main(String[] args) {
